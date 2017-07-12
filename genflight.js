4a901ea75350f6b7d2886e45a71fd6a7cdef9800
Brin Brody <825orion@gmail.com>
2017-07-12 12:51:12 -0400

    Comments, random squawk codes

--- genflight.js
+++ genflight.js
@@ -1,10 +1,11 @@
 function genFlight(lat,lng,altitude,hdg)
 {
-    var callsign = newAircraft();
-    var squawk = 3001;
+    var callsign = newAircraft(); //Yes, we even make their callsign for you.  See generatecallsign.js for a list of airlines.
+    var randSq = Math.ceil(Math.random() * 10); 
+    var squawk = 7777+randSq*1000; //Every aircraft has a randomised squawk.
     // Spacer
     if(!heading){
-        var heading = 010;
+        var heading = 360; //Standard hdg.  We start North.
     }else{
         var heading = hdg;
     }
@@ -13,22 +14,22 @@ function genFlight(lat,lng,altitude,hdg)
     }else{
         var alt = altitude; //Pulls from the altitude input.
     }
-    var gs = 1;
-    var cdr = 1800;
+    var gs = 1; //Ground Speed
+    var cdr = 1800; //Climb/Descent Rate
     var type = "I"; // IFR (I) or VFR (V)
     // Newline
-    var actype = genICAO();
+    var actype = genICAO(); 
     var cruisespeed = 400;
-    var depaprt = "KJFK";
+    var depaprt = "KJFK"; //Change to change route start points.
     var deptime = 1755;
-    var randNum = Math.ceil(Math.random() * 10);
-    var cruisealt = 30000 + randNum * 1000;
-    var arraprt = genAirport();
+    var randNum = Math.ceil(Math.random() * 10); 
+    var cruisealt = 30000 + randNum * 1000; //Unless specified in the PRD, randomly generated.
+    var arraprt = genAirport(); //Generated randomly
     var enroutehrs = 4;
     var enroutemins = 50;
     // Newline
-    var routeData = getRoute(depaprt, arraprt);
-    var route = "DIRECT";
+    var routeData = getRoute(depaprt, arraprt); //Get PRD info
+    var route = "DIRECT"; //If there's no entry in the PRD, go direct.
     
     if (routeData != null)
     {
@@ -39,13 +40,13 @@ function genFlight(lat,lng,altitude,hdg)
         }
     }
     
-    var voicetype = "/v/";
+    var voicetype = "/v/"; //Because trainers are /v/
     
     // AAL123:B190/A:T:V:KBTV:KMHT:15500:BTV MPV LEB MHT:/V/VFR/CHARTS:1200:S:44.46370:-73.15221:335:0:360
     // Callsign:Type:Engine:Rules:Dep Field:Arr Field:Crz Alt:Route:Remarks:Sqk Code:Sqk Mode:Lat:Lon:Alt:Speed:Heading
     
     var out = callsign + ":" + actype + "/L:J:" + type + ":" + depaprt + ":" + arraprt + ":" + 
         cruisealt + ":" + route + ":" + voicetype + ":" + squawk + ":S:" + lat + ":" + lng + ":" +
-        alt + ":" + gs + ":" + heading;
+        alt + ":" + gs + ":" + heading; //Please don't mess with this.  This is what makes your file work.
     return out;
 }
